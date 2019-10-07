269301210ce137c543a5e02208cef7fa09151a08
r-firdovsi <r-firdovsi@hotmail.com>
2019-10-07 16:35:00 +0400

    I make Image Upload Feature and Create Signup, Delete User Routes

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb://localhost:27017/node-rest-shop',  { useNewUrlParser: true,  useUnifiedTopology: true  });
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -30,6 +32,7 @@ app.use((req, res, next) => {
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
